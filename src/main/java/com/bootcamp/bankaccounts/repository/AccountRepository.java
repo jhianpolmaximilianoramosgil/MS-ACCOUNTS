eb9ce4fd7a87bd6cedf045aae76bbfee6431c57f
jhianpolmaximilianoramosgil <jhianpol.ramos@vallegrande.edu.pe>
2023-04-02 21:07:52 -0500

    Update

--- src/main/java/com/bootcamp/bankaccounts/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.bankaccounts.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.bootcamp.bankaccounts.entity.Account;
-
-public interface AccountRepository extends ReactiveMongoRepository<Account, String> {
-
-}
--- src/main/java/com/bootcamp/bankaccounts/repository/YankiRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.bankaccounts.repository;
-
-import com.bootcamp.bankaccounts.entity.Yanki;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface YankiRepository extends ReactiveMongoRepository<Yanki, String> {
-
-}
